7288cdd7d92322a390140bfaf2cfc995d38dd930
Emanuele 'Lele' Calo <lele@sshadm.in>
2019-05-15 02:04:27 +0200

    Fixed cert files variable in config #2

--- monitoring/checkers.go
+++ monitoring/checkers.go
@@ -76,9 +76,9 @@ func PingHealth(serfRPCAddr, serfMemberName string) (c health.Checker, err error
 func TimeSkewHealth(rpcAddrs []string, agentCAfile, agentCertFile, agentKeyFile, serfRPCAddr, serfMemberName string) (c health.Checker, err error) {
 	c, err = NewTimeSkewChecker(TimeSkewCheckerConfig{
 		RPCAddrs:       rpcAddrs,
-		agentCAFile:    agentCAfile,
-		agentCertFile:  agentCertFile,
-		agentKeyFile:   agentKeyFile,
+		AgentCAFile:    agentCAfile,
+		AgentCertFile:  agentCertFile,
+		AgentKeyFile:   agentKeyFile,
 		SerfRPCAddr:    serfRPCAddr,
 		SerfMemberName: serfMemberName,
 	})
